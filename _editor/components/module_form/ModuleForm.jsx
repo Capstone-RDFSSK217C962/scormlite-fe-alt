a8ef2d0dbba41697218a5128a197136118a9650b
Virginia Ritonga <virginia.p.m@mail.ugm.ac.id>
2022-06-20 01:47:53 +0700

    fix(module-form): delete unused code

--- _editor/components/module_form/ModuleForm.jsx
+++ _editor/components/module_form/ModuleForm.jsx
@@ -13,19 +13,6 @@ class ModuleForm extends Component {
         super(props);
     }
 
-    // componentDidMount () {
-    //     CourseService.getCourseById(this.props.match.params.id).then(
-    //         response => {
-    //             if ('module' in response.data) {
-    //                 this.props.dispatch(importState(serialize(response.data.module.undoGroup)));
-    //             }
-    //         },
-    //         error => {
-    //             return error;
-    //         }
-    //     );
-    // }
-
     render() {
         return (
             <div className="wrapperDetail">
@@ -35,12 +22,6 @@ class ModuleForm extends Component {
                     <Link to={`editor/${this.props.id}`}>
                         <a>Tambah Modul</a>
                     </Link>
-                    {/* <Visor id="visor"
-                    title={title}
-                    visorVisible={true}
-                    // onVisibilityToggled={()=> this.setState({ visorVisible: !this.state.visorVisible })}
-                    filesUploaded={this.props.store.getState().filesUploaded }
-                    state={{ ...this.props.store.getState(), filesUploaded: this.props.store.getState().filesUploaded, status: this.props.store.getState().status }}/> */}
                 </div>
             </div>
         );
