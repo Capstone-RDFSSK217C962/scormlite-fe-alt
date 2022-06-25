a1890150bff051c026db937bc5735267e769ca6e
victoriatrinita <victoriapardede@gmail.com>
2022-06-25 17:52:50 +0700

    remove `es` locale

--- locales/i18n.js
+++ locales/i18n.js
@@ -1,14 +1,13 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import { es } from './es.js';
 import { en } from './en.js';
 
-let translations = { en: en, es: es };
+let translations = { en: en };
 
 i18n
     .use(LanguageDetector)
     .init({
-        whitelist: ['es', 'en'],
+        whitelist: ['en'],
         fallbackLng: 'en',
         attributes: ['t', 'i18n'],
         resources: translations,
