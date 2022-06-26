b7f706c6d00919c915654879c6969387cc4dbc5e
Virginia Ritonga <virginia.p.m@mail.ugm.ac.id>
2022-06-26 16:06:32 +0700

    fix(server): change path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let app = express();
 app.use(history({
     verbose: true,
 }));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/dist/')));
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
